273d5ce35005b31f20be02cde13d28d30a929e87
Italo Abreu <italodw@gmail.com>
2019-10-28 00:53:31 -0300

    Mostrando Lembretes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,45 @@
 <template>
-  <div class="home">
-    <h1>Home Page</h1>
+  <div>
+    <h1>Lembretes</h1>
+    <v-card
+      class="mx-5 my-5"
+      max-width="344"
+      outlined
+      v-for="card in cards"
+      :key="card.id"
+    >
+      <v-card-title>{{ card.title }}</v-card-title>
+      <v-card-subtitle>{{ `Para ${card.calendar} as ${card.time}` }}</v-card-subtitle>
+      <v-card-text>{{ card.description }}</v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import api from "../services/api";
 
 export default {
-  name: 'home',
-}
+  data() {
+    return {
+      cards: null
+    };
+  },
+  methods: {
+    reverseObj(obj) {
+      const arryReverse = Object.entries(obj).reverse();
+      const objReverse = {};
+      arryReverse.forEach(element => {
+        objReverse[element[0]] = element[1];
+      });
+      return objReverse;
+    },
+    async getLembretes() {
+      let cards = await api.get("/lembretes");
+      return (this.cards = this.reverseObj(cards.data));
+    }
+  },
+  mounted() {
+    this.getLembretes();
+  }
+};
 </script>
